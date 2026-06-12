95cca40470e251e344a16ca4e950302b83156a9a
khatry <khatrycheikh79@gmail.com>
2026-06-12 16:31:30 +0000

    modified for part QUIZ

--- labo_performance/urls.py
+++ labo_performance/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('labo/', views.labo, name='labo'),
     path('quiz/', views.quiz, name='quiz'),
     path('a-propos/', views.a_propos, name='a_propos'),
+    path('learn/', views.cours_liste, name='cours_liste'),
+    path('learn/<int:cours_id>/', views.cours_detail, name='cours_detail'),
 ]
